962745fe2046ff3730b7caea8ae3726056fac36d
gache <erickfrancodelgado@hotmail.com>
2024-07-15 11:56:13 +0200

    implementation de la methode getAllProducts

--- src/main/java/com/alten/test/apiproduct/service/ProductServiceImpl.java
+++ src/main/java/com/alten/test/apiproduct/service/ProductServiceImpl.java
@@ -1,14 +1,23 @@
 package com.alten.test.apiproduct.service;
 
 import com.alten.test.apiproduct.model.Product;
+import com.alten.test.apiproduct.repository.ProductRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class ProductServiceImpl implements ProductServiceInter {
+
+
+    public final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
     @Override
     public List<Product> getAllProducts() {
-        return List.of();
+        return productRepository.findAll();
     }
 
     @Override
